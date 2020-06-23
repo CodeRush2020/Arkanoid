92da56fd3820da6a0c68d72016f35ab6f1ba44c7
FrancoMolina <francoeliasmolina@hotmail.com>
2020-06-23 15:39:03 -0300

    con MVC

---
+++ eclipse-workspace-java/ArkanoidMVC/src/ArkanoidMVC.java
@@ -0,0 +1,17 @@
+import Model.Model;
+import View.View;
+import Controller.Controller;
+
+public class ArkanoidMVC {
+    public static void main(String[] args) {
+        Model model = new Model();
+        View view = new View(model);
+        Controller controller = new Controller(model, view);
+        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                view.setVisible(true);
+            }
+        });
+    }
+}
